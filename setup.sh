f7a596339ece73a7d485cbe95ac128a52d504617
Luke <colored.vertices@gmail.com>
2025-04-21 21:01:22 -0300

    fix: Pequenas mudanças v1.1.1
    Adicionados echos em ambos os arquivos
    Alguns comentários foram reescritos para
    maior clareza

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@
 
 declare -a usuarios=( carlos maria joao debora sebastiana roberto josefina amanda rogerio )
 
-# Criação dos grupos
+echo "Criação dos grupos"
 groupadd GRP_ADM
 groupadd GRP_VEN
 groupadd GRP_SEC
 
-# Criação dos usuários
+echo "Criação dos usuários"
 # adm
 useradd carlos 	-c "Carlos" -m -G GRP_ADM -s /bin/bash -p $(openssl passwd -6 12345)
 useradd maria 	-c "Maria" 	-m -G GRP_ADM -s /bin/bash -p $(openssl passwd -6 12345)
@@ -23,21 +23,21 @@ useradd josefina -c "Josefina"	-m -G GRP_SEC -s /bin/bash -p $(openssl passwd -6
 useradd amanda	 -c "Amanda"	-m -G GRP_SEC -s /bin/bash -p $(openssl passwd -6 12345)
 useradd rogerio	 -c "Rogerio"	-m -G GRP_SEC -s /bin/bash -p $(openssl passwd -6 12345)
 
-# Tornando a senha descartável
+echo "Alterando a senha para ser usada apenas no primeiro login"
 for i in "${usuarios[@]}"
 do
 	passwd $i -e
 done
 unset usuarios
 
-# Criando diretórios
+echo "Criando diretórios"
 mkdir /publico /adm /ven /sec
 
-# Atualizando permissões
+echo "Atualizando permissões"
 chmod 777 /publico
 chown root:GRP_ADM /adm
 chown root:GRP_VEN /ven
 chown root:GRP_SEC /sec
 chmod 770 /adm /ven /sec
 
-# Finalizado
+echo "Finalizado"
